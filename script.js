173cb6749bd0869fcb9d20997e6671da620826c5
Maria Catarina <115542470+MariaCatarinaOliveira@users.noreply.github.com>
2023-02-07 15:10:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let nome = "Maria";
+let idade = 20;
+console.log(nome);
+console.log(idade);
+
+let idade7 = idade*7;
+console.log(idade7);
+
+let besties = ["Zé","Sara","Nadine","Lisa"];
+console.log(besties);
+
+let eu = {
+    name: "Maria",
+    age: 20,
+    isSlay: true,
+    isStudent: true,
+    isGoofy: true
+};
+console.log(eu);
+
+let bestiesArray = 
+[
+    {
+        name: "Zé",
+        age: 20,
+        isSlay: true,
+        isStudent: true,
+        isGoofy: true
+    },
+    {
+        name: "Sara",
+        age: 20,
+        isSlay: true,
+        isStudent: true,
+        isGoofy: true
+    },
+    {
+        name: "Nadine",
+        age: 20,
+        isSlay: true,
+        isStudent: false,
+        isGoofy: true
+    },
+    {
+        name: "Lisa",
+        age: 26,
+        isSlay: true,
+        isStudent: false,
+        isGoofy: true
+    }
+];
+console.log(bestiesArray);
